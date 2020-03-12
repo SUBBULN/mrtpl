b522ab9b32d150009b5306363f1a0be9f79eb173
subbareddy <subbareddy@zirafftechnologies.com>
2020-03-12 15:15:23 +0530

    added website

---
+++ precache-manifest.30e8ce1f1a341abdcb42b8fb4241707c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788c5ed0fc788d539a99aadcec088e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0083051037f632b26125",
+    "url": "/static/css/2.54081f38.chunk.css"
+  },
+  {
+    "revision": "4c56f326c28f674e0f06",
+    "url": "/static/css/main.6476549b.chunk.css"
+  },
+  {
+    "revision": "0083051037f632b26125",
+    "url": "/static/js/2.b81a83d7.chunk.js"
+  },
+  {
+    "revision": "14d5961f69d2d17816fa3e768ff6a765",
+    "url": "/static/js/2.b81a83d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c56f326c28f674e0f06",
+    "url": "/static/js/main.c439d322.chunk.js"
+  },
+  {
+    "revision": "4bbebc210cdba1e5b6e2",
+    "url": "/static/js/runtime-main.bdb8250a.js"
+  }
+]);
